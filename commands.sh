394a269d0c4f096b0dd901b5891615f2dd9b4733
mcalavera81 <farid.hafhafi@fonetic.com>
2017-06-27 19:19:12 +0200

    sum of coupling

--- commands.sh
+++ commands.sh
@@ -6,3 +6,6 @@ maat -l hib_evo.log -c git -a revisions > hib_freqs.csv
 python ~/Development/ixmaat0.8.5/scripts\ 4/merge_comp_freqs.py hib_freqs.csv hib_lines.csv
 python ~/Development/ixmaat0.8.5/scripts\ 4/csv_as_enclosure_json.py --structure hib_lines.csv  --weights output.csv > output.json
 python -m SimpleHTTPServer 8888
+python ~/Development/ixmaat0.8.5/scripts\ 4/complexity_analysis.py hibernate-core/src/main/java/org/hibernate/cfg/Configuration.java
+python ~/Development/ixmaat0.8.5/scripts\ 4/git_complexity_trend.py   --start ccc087b --end 46c962e   --file hibernate-core/src/main/java/org/hibernate/cfg/Configuration.java
+maat -l hib_evo.log -c git -a soc > sum_of_coupling.csv
